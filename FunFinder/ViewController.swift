59fe2eaa230e5c9ac5b074eb8c6b72ac7d8fed1e
Ashley Minor <ashleyminor07@gmail.com>
2019-05-18 13:53:52 -0600

    Initial Commit

---
+++ FunFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFinder
+//
+//  Created by Ashley Minor on 5/18/19.
+//  Copyright © 2019 Ashley Minor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
